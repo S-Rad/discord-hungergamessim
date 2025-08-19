3f337e4dd285668ab112aa5e4a21695ec73bcf64
S-Rad <42512223+S-Rad@users.noreply.github.com>
2025-08-19 13:06:20 +0200

    Init

--- src/main/java/com/SRad/discordbot/BotTokenStorage.java
+++ src/main/java/com/SRad/discordbot/BotTokenStorage.java
@@ -1,5 +1,5 @@
 package com.SRad.discordbot;
 
 public final class BotTokenStorage {
-    public static String BotToken = "MTQwNjE5Njc3Njc2NzA2NjEzMw.Gho5q5.ykMlQgKZHnrwaUjSxAO-PDv1-PvYvNLmVYnH-U";
+    public static String BotToken = "fakeToken";
 }
