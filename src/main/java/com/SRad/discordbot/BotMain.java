38f7384df3066390d6fac6673578e928f3b31655
S-Rad <42512223+S-Rad@users.noreply.github.com>
2025-08-24 16:47:24 +0200

    Main Class reads Bot token from Properties file now

--- src/main/java/com/SRad/discordbot/BotMain.java
+++ src/main/java/com/SRad/discordbot/BotMain.java
@@ -1,19 +1,22 @@
 package com.SRad.discordbot;
 
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
 import net.dv8tion.jda.api.sharding.ShardManager;
 
 import javax.security.auth.login.LoginException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
 
 public class BotMain {
     private final ShardManager shardManager;
 
     public BotMain() throws LoginException{
-        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(BotTokenStorage.BotToken);
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(getBotToken());
         builder.setStatus(OnlineStatus.ONLINE);
         builder.setActivity(Activity.watching("you"));
         shardManager = builder.build();
@@ -32,6 +35,16 @@ public class BotMain {
             System.out.println("Login Failed!");
         }
 
+    }
 
+    public String getBotToken(){
+        Properties properties = new Properties();
+        try (InputStream input = Files.newInputStream(Paths.get("src/config.properties"))){
+            properties.load(input);
+
+        }catch(IOException e){
+            System.err.println("fehlgeschlagen: " + e.getMessage());
+        }
+        return properties.getProperty("bot.token", "fail");
     }
 }
